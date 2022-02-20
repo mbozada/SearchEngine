570889831eef2320051f93221eac2c1022b66ff6
mbozada <52613673+mbozada@users.noreply.github.com>
2022-02-20 14:14:33 -0500

    Created SearchEngine.java
    Added a couple lines describing milestones.

---
+++ SearchEnginge.java
@@ -0,0 +1,14 @@
+public class SearchEnginge {
+    
+}
+
+// Milestones
+// Prepare the corpus according to the input requirements of the Lucene API.
+
+// Implement Indexer to index all the documents in the corpus and store the index on disk (of the local machine).
+
+// Implement Query Parser to process the incoming queries.
+
+// Implement Index Searcher to satisfy the user query and retrieve the relevant documents.
+
+// Enhance the search results and the search engine performance by modifying Lucene API parameters
