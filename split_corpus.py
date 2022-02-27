8a69b56d5816b8611f7295b3abec466c54bbd2e3
mbozada <52613673+mbozada@users.noreply.github.com>
2022-02-26 19:01:31 -0500

    Created corpus_data.txt using split_corpus.py
    corpus_data.txt follows the format of title string\n followed by abstract string\n

--- split_corpus.py
+++ split_corpus.py
@@ -1,9 +1,64 @@
-with open('./corpus/cran.all.1400') as f:
-    lines = f.read()
-    split_file = lines.split('.I ')
+title_count = 0
+abstract_count = 0
 
-    for i in range(1,1401):
-        filename = "./split_corpus/" + str(i)
-        new_file = open(filename, 'w')
-        new_file.write(split_file[i])
-        new_file.close()
+# Shouldn't work, but it does
+with open('./corpus/cran.all.1400') as f:
+    lines = f.readlines()
+    join_titles = False
+    title = ""
+    title_list = []
+    join_abstracts = False
+    abstract = ""
+    abstract_list = []
+    for line in lines:
+        
+        # Titles
+        if line.strip() == ".A":
+            if join_titles:
+                title_list.append(title)
+                title = ""
+            join_titles = False
+            
+
+        if join_titles:
+            title += line.replace("\n"," ").replace("\r"," ").replace(" .","")
+
+        if line.strip() == ".T":
+            join_titles = True
+            title_count += 1
+
+        # Abstracts
+        if '.I' in line:
+            if join_abstracts:
+                abstract_list.append(abstract)
+                abstract = ""
+            join_abstracts = False
+            
+            
+
+        if join_abstracts:
+            abstract += line.replace("\n"," ").replace("\r"," ").replace(" .","")
+
+        if line.strip() == ".W":
+            join_abstracts = True
+            abstract_count += 1
+
+# Reveals that the counts are off. If you put the title count and abstract count in front
+# of the default "" reset for title and abstract, you'll find they eventually drift.
+# However, the title and abstract match appropriately in almost all cases. Very weird.
+
+# print(title_count)
+# print(abstract_count)
+# print(title_list[0])
+# print(abstract_list[0])
+# print(len(title_list))
+# print(len(abstract_list))
+
+with open ('corpus_data.txt', 'w') as f:
+    for i in range(len(abstract_list)):
+        f.write(title_list[i] + '\n')
+        f.write(abstract_list[i] + '\n')
+
+    
+
+
