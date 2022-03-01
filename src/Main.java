d0036d9ba4e5baeb29707a2a454bf1dc0feba84b
Quinn Hirt <hirtquinn@gmail.com>
2022-02-28 19:49:13 -0500

    sharing code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -21,7 +23,7 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class Main {
-	public static void main(String[] args) throws IOException, ParseException {
+	public static ArrayList<Integer> ourSearch(String our_query) throws IOException, ParseException {
 		// 0. Specify the analyzer for tokenizing text.
 		// The same analyzer should be used for indexing and searching
 		StandardAnalyzer analyzer = new StandardAnalyzer();
@@ -62,20 +64,14 @@ public class Main {
 		
 
 		// 2. query
-		Scanner sin = new Scanner(System.in);
-		String fieldString;
-		String querystr;
 		while(true) {
-			System.out.println("Name Field to Search: ");
-			fieldString = sin.nextLine();
-			System.out.println("Content to Search: ");
-			querystr = sin.nextLine();
+			
 				// the "title" arg specifies the default field to use
 			// when no field is explicitly specified in the query.
 			QueryParser parser = new QueryParser("title", analyzer);
 			org.apache.lucene.search.Query q;
 			try {
-				q = parser.parse(querystr);
+				q = parser.parse(our_query);
 			}
 			// I AM SO SORRY
 			catch (Exception e) {
@@ -89,11 +85,13 @@ public class Main {
 			IndexSearcher searcher = new IndexSearcher(reader);
 			TopDocs docs = searcher.search(q, hitsPerPage);
 			ScoreDoc[] hits = docs.scoreDocs;
+			ArrayList<Integer> hits_arr = new ArrayList<Integer>();
 
 			// 4. display results
 			System.out.println("Found " + hits.length + " hits.");
 			for (int i = 0; i < hits.length; ++i) {
 				int docId = hits[i].doc;
+				hits_arr.add(docId);
 				org.apache.lucene.document.Document d = searcher.doc(docId);
 				// System.out.println((i + 1) + ". \t" + d.get("doc_id") + ".\t" + d.get("title") + "\t\t\t" + d.get("contents"));
 				System.out.println(d.get("doc_id"));
@@ -102,7 +100,45 @@ public class Main {
 			// reader can only be closed when there
 			// is no need to access the documents any more.
 			reader.close();
+			return hits_arr;
 			}
 		
+	}
+
+	public static void main(String[] arggggggggggggggggggggggg) throws IOException, ParseException {
+		ArrayList<Integer> query_ids = new ArrayList<Integer>();
+		ArrayList<String> queries = new ArrayList<String>();
+		File f = new File("myQuery.txt");
+		Scanner fin = new Scanner(f);
+		while(fin.hasNextLine()) {
+			query_ids.add(Integer.parseInt(fin.nextLine()));
+			queries.add(fin.nextLine());
+		}
+		// System.out.println(truth_ids);
+		// System.out.println(turth_queries);
+		ArrayList<ArrayList<Integer>> guess_ids = new ArrayList<ArrayList<Integer>>();
+		for(String query: queries) {
+			guess_ids.add(ourSearch(query));
+		}
+
+		f = new File("myQueryRels.txt");
+		fin = new Scanner(f);
+		ArrayList<ArrayList<Integer>> rel_arr = new ArrayList<ArrayList<Integer>>();
+		while(fin.hasNextLine()) {
+			String nums = fin.nextLine();
+			String[] nums_to_str_arr = nums.split(" ");
+			ArrayList<Integer> rels = new ArrayList<Integer>();
+			for(int i = 0; i < nums_to_str_arr.length; i++) {
+				if(i != 0) {
+					rels.add(Integer.parseInt(nums_to_str_arr[i]));
+				}
+				else {
+					// empty else
+				}
+			}
+			rel_arr.add(rels);
+			System.out.println(rel_arr);
+		}
+
 	}
 }
